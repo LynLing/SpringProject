04776644c138a3d071cf0cf3ec5c6d3484615801
LynLing <finitytype@gmail.com>
2018-05-25 22:09:01 -0400

    update simulator & distribution service

---
+++ prj4/running-location-simulator/src/main/java/demo/SimulatorServiceApplication.java
@@ -0,0 +1,25 @@
+package demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@SpringBootApplication
+@EnableScheduling
+public class SimulatorServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SimulatorServiceApplication.class, args);
+    }
+
+    //schedualer implememnts Sping's taskExcutor
+    @Bean
+    public AsyncTaskExecutor taskExecutor(){
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        return scheduler;
+    }
+}
---
+++ prj4/running-location-simulator/src/main/java/demo/service/impl/DefaultPositionService.java
@@ -0,0 +1,30 @@
+package demo.service.impl;
+
+import demo.model.CurrentPosition;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import demo.service.PositionService;
+
+@Slf4j
+
+@Service
+public class DefaultPositionService implements PositionService {
+
+
+    //    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultPositionService.class);
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${com.lyn.running.location.distribution}")
+    private String runningLocationDistribution;
+
+    @Override
+    public void processPositionInfo(long id, CurrentPosition currentPosition, boolean sendPositionsToDistributionService) {
+        //runningLocationDistribution = "http://localhost:9006";
+        if(sendPositionsToDistributionService){
+            log.info(String.format("Thread %d Simulator is calling distribution Rest API", Thread.currentThread().getId()));
+            this.restTemplate.postForLocation(runningLocationDistribution + "/api/location", currentPosition);
+        }
+    }
+}
